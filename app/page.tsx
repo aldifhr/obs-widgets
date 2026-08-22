04feb0710ef0c0b75b4f6925d4a4fc74b1331460
aldifhr <aldifahriziilmawan@gmail.com>
2026-08-22 14:57:47 +0700

    chore: remove pixel battle

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,6 @@ export const listOverlay = [
     name: 'Starting Soon',
     description: 'Animasi text fullscreen + countdown',
     href: '/starting',
-  },
-  {
-    name: 'Pixel Battle',
-    description: 'Viewer TikTok jadi karakter pixel berantem • max 10',
-    href: '/battle',
   }
 ]
 export default function HomePage() {
